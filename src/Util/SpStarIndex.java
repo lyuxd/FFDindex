2b3f1fbb04f5a882bb575a802886ca68a8b50496
lyuxd <lxd.1990@gmail.com>
2015-04-01 22:18:06 +0800

    Move form Query package.

---
+++ src/Util/SpStarIndex.java
@@ -0,0 +1,12 @@
+package Util;
+
+
+public class SpStarIndex implements Index {
+
+	@Override
+	public Tuple getValueForKey(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
