317890e39c4e217a71fbfc1b35a8509ce28a009e
lyuxd <lxd.1990@gmail.com>
2015-04-01 19:15:03 +0800

    The corresponding class for tuples of neighbothood table.

--- src/Util/NeighborhoodTableTuple.java
+++ src/Util/NeighborhoodTableTuple.java
@@ -1,5 +1,29 @@
 package Util;
 
-public class NeighborhoodTableTuple {
+import java.util.HashSet;
 
+public class NeighborhoodTableTuple {
+	private String centralVertexLable;
+	private String FgPrt;
+	private HashSet<Branch> AdjSet;
+	
+	
+	public String getCentralVertexLable() {
+		return centralVertexLable;
+	}
+	public void setCentralVertexLable(String centralVertexLable) {
+		this.centralVertexLable = centralVertexLable;
+	}
+	public String getFgPrt() {
+		return FgPrt;
+	}
+	public void setFgPrt(String fgPrt) {
+		FgPrt = fgPrt;
+	}
+	public HashSet<Branch> getAdjSet() {
+		return AdjSet;
+	}
+	public void setAdjSet(HashSet<Branch> adjSet) {
+		AdjSet = adjSet;
+	}
 }
