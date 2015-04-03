26f57177f8cd425fd512333c741a402b17db8c9a
lyuxd <lxd.1990@gmail.com>
2015-04-03 22:33:35 +0800

    Add the inheritance relationship.

--- src/Util/NeighborhoodTableTuple.java
+++ src/Util/NeighborhoodTableTuple.java
@@ -1,29 +1,54 @@
 package Util;
 
-import java.util.HashSet;
+import java.util.Set;
 
-public class NeighborhoodTableTuple {
+public class NeighborhoodTableTuple extends IndexTupleAbstract{
 	private String centralVertexLable;
 	private String FgPrt;
-	private HashSet<Branch> AdjSet;
-	
-	
+	private Set<String> AdjSet;
+
 	public String getCentralVertexLable() {
 		return centralVertexLable;
 	}
+
 	public void setCentralVertexLable(String centralVertexLable) {
 		this.centralVertexLable = centralVertexLable;
 	}
+
 	public String getFgPrt() {
 		return FgPrt;
 	}
+
 	public void setFgPrt(String fgPrt) {
 		FgPrt = fgPrt;
 	}
-	public HashSet<Branch> getAdjSet() {
+
+	public Set<String> getAdjSet() {
 		return AdjSet;
 	}
-	public void setAdjSet(HashSet<Branch> adjSet) {
-		AdjSet = adjSet;
+
+	public void setAdjSet(Set<String> set) {
+		AdjSet = set;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder tostring = new StringBuilder();
+		tostring.append("Central Vertex: " + centralVertexLable + "\n"
+				+ "Fingerprint: " + FgPrt + "\n" + "AdjEdges: \n");
+		if (AdjSet != null && AdjSet.size() != 0) {
+			for (String s : AdjSet) {
+				tostring.append(s + "\n");
+			}
+		}else{
+			tostring.append("null\n");
+		}
+		return tostring.toString();
+	}
+
+	@Override
+	public boolean init(String line) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
